de137cab631bd799bc2efb3609070a35359965a6
cbaker9 <145694197+cbaker9@users.noreply.github.com>
2023-12-13 16:05:09 -0600

    Update plot1.R
    update

--- plot1.R
+++ plot1.R
@@ -3,5 +3,7 @@ library(readr)
 #Import data - NOTE: "..." in file path to hide personal information
 household_power_consumption <- read_delim(".../household_power_consumption.txt", ";", escape_double = FALSE, trim_ws = TRUE)
 
-#Subset data to Dates "1/2/2007" and "2/2/2007"
+#Subset data to Dates "2/1/2007" and "2/2/2007"
 subset_data <- subset(household_power_consumption, Date == c("1/2/2007", "2/2/2007"))
+
+hist(household_power_consumption$Global_active_power, col = "red" , main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", xaxis = seq(0,2,4,6), ylim = c(0,1200))
