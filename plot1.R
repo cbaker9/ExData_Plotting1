e3f21b928f5914b31b2ba8e4471cfb7fcc7c1dc3
cbaker9 <145694197+cbaker9@users.noreply.github.com>
2023-12-14 07:32:19 -0600

    Update plot1.R
    s/b final code

--- plot1.R
+++ plot1.R
@@ -1,9 +1,14 @@
 library(readr)
 
-#Import data - NOTE: "..." in file path to hide personal information
+# Import data - NOTE: "..." in file path to hide personal information
 household_power_consumption <- read_delim(".../household_power_consumption.txt", ";", escape_double = FALSE, trim_ws = TRUE)
 
-#Subset data to Dates "2/1/2007" and "2/2/2007"
-subset_data <- subset(household_power_consumption, Date == c("1/2/2007", "2/2/2007"))
+# Subset data to Dates "2/1/2007" and "2/2/2007"
+subset_data <- subset(household_power_consumption, Date == c("2/1/2007", "2/2/2007"))
 
-hist(household_power_consumption$Global_active_power, col = "red" , main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", xaxis = seq(0,2,4,6), ylim = c(0,1200))
+# Plot the histogram
+hist(subset_data$Global_active_power, col = "red" , main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+
+# Save as .png file
+dev.copy(png, file = "plot1.png", height = 480, width = 480)
+dev.off()
