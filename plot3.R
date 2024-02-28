08f16f06b1549c55fb36c1ce1d00c6e135e4b5f2
cbaker9 <145694197+cbaker9@users.noreply.github.com>
2024-02-28 10:12:08 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,12 +1,31 @@
+#Libraries used:
 library(readr)
 library(lubridate)
 
-# Plot2
-
+#Import the dataset
 household_power_consumption <- read_delim("household_power_consumption.txt", 
                                           +     ";", escape_double = FALSE, col_types = cols(Date = col_character()), 
                                           +     trim_ws = TRUE)
 
+#Subset data to specific days
 sub_data <- subset(household_power_consumption, Date == c("1/2/2007", "2/2/2007"))
 
+#Converting and merging Date and Time
 sub_data$DateTime <- strptime(paste(sub_data$Date, sub_data$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+
+# Plot the data and save .png
+plot(sub_data$DateTime, sub_data$Sub_metering_1 , type = "l", col = "black", ylab = "Energy sub metering", xlab = " ")
++lines(sub_data$DateTime, sub_data$Sub_metering_2, col = 'red')
++lines(sub_data$DateTime,sub_data$Sub_metering_3 , col = 'blue')
+
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
+dev.off()
+
+
+
+
+
+
+
+
+
